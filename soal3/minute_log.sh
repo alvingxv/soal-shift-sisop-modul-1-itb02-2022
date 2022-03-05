aacd2fabbea4bd63fed75a4ef498375ac143cca4
Satriokml <satriokamil2@gmail.com>
2022-03-05 13:22:40 +0000

    Update minute_log.sh

--- soal3/minute_log.sh
+++ soal3/minute_log.sh
@@ -5,7 +5,8 @@ swap=$(free -m | awk '/^Swap/ {print $2","$3","$4}')
 path=$(du -sh /home/satrio/ | awk '{print $2","$1}')
 namafile="metrics_$(date +"%Y%m%d%I%M%S").log"
 echo "$memory,$swap,$path" >> /home/satrio/log/$namafile
+echo "$memory,$swap,$path" >> /home/satrio/log/temporary.txt 
 cd /home/satrio/log/
-chown satrio metrics_2022*
-chmod 400 metrics_2022*
-
+chown satrio metrics_2*
+chmod 400 metrics_2*
+rm metrics.log
